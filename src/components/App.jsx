90af89e0319039fe2d1fdd942c5c08b4a3757913
uutbudiarto2012 <hikids.developer@gmail.com>
2021-11-30 12:10:05 +0700

    adding particles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Particles from 'react-tsparticles';
 import Hero from './Hero/Hero';
 import About from './About/About';
 import Projects from './Projects/Projects';
@@ -17,6 +18,16 @@ function App() {
   const [contact, setContact] = useState({});
   const [footer, setFooter] = useState({});
 
+  const particlesInit = (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   useEffect(() => {
     setHero({ ...heroData });
     setAbout({ ...aboutData });
@@ -27,6 +38,91 @@ function App() {
 
   return (
     <PortfolioProvider value={{ hero, about, projects, contact, footer }}>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: '#000',
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: 'push',
+              },
+              onHover: {
+                enable: true,
+                mode: 'repulse',
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.01,
+                size: 4,
+              },
+              push: {
+                quantity: 1,
+              },
+              repulse: {
+                distance: 100,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              // value: "#ebebeb",
+              value: ['#fbb03b', '#ffffff'],
+            },
+            links: {
+              color: '#000',
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 0.5,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: 'none',
+              enable: true,
+              outMode: 'bounce',
+              random: false,
+              speed: 1,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 180,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: 'circle',
+              width: 0,
+              color: '#000000',
+            },
+            size: {
+              random: true,
+              value: 2,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
       <Hero />
       <About />
       <Projects />
